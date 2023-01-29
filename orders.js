cbad9a8dbcc1765d652136972bbc0277d3090849
wayan2602 <23402010004@student.prasetiyamulya.ac.id>
2023-01-29 23:40:43 +0700

    second commit

---
+++ orders.js
@@ -0,0 +1,74 @@
+const Orders = [
+  {
+    productName: "Mini Camera",
+    productNumber: "1232",
+    paymentStatus: "Due",
+    shipping: "Decline",
+  },
+  {
+    productName: "Mini Drone",
+    productNumber: "12323",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "Mini Gopro",
+    productNumber: "12325",
+    paymentStatus: "Due",
+    shipping: "Decline",
+  },
+  {
+    productName: "Mini Camera",
+    productNumber: "1232",
+    paymentStatus: "Due",
+    shipping: "Decline",
+  },
+  {
+    productName: "Mini Drone",
+    productNumber: "12323",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "Mini Gopro",
+    productNumber: "12325",
+    paymentStatus: "Due",
+    shipping: "Decline",
+  },
+  {
+    productName: "Mini Camera",
+    productNumber: "1232",
+    paymentStatus: "Due",
+    shipping: "Decline",
+  },
+  {
+    productName: "Mini Drone",
+    productNumber: "12323",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "Mini Gopro",
+    productNumber: "12325",
+    paymentStatus: "Due",
+    shipping: "Decline",
+  },
+  {
+    productName: "Mini Camera",
+    productNumber: "1232",
+    paymentStatus: "Due",
+    shipping: "Decline",
+  },
+  {
+    productName: "Mini Drone",
+    productNumber: "12323",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "Mini Gopro",
+    productNumber: "12325",
+    paymentStatus: "Due",
+    shipping: "Decline",
+  },
+];
